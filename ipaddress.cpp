ff114c2babe79614d79806f52b5b27e33628d0ef
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-10-16 23:38:58 -0700

    dfhg

--- ipaddress.cpp
+++ ipaddress.cpp
@@ -33,22 +33,11 @@ void showAddresses(SSD1306::OledI2C& oled){
             void *tmpAddrPtr = &((struct sockaddr_in *)ifa_addr)->sin_addr;
             char addressBuffer[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-            std::string address;
-            if (strncmp(ifa->ifa_name, "eth", 3) == 0)
+            if (strncmp(ifa->ifa_name, "lo", 2) != 0)
             {
-                address = "E";
+                SSD1306::OledPoint location{0, 16};
+                drawString8x16(location, addressBuffer, SSD1306::PixelStyle::Set, oled);
             }
-            else if (strncmp(ifa->ifa_name, "wlan", 4) == 0)
-            {
-                address = "W";
-            }
-            else if (strncmp(ifa->ifa_name, "usb", 3) == 0)
-            {
-                address = "U";
-            }
-            address += addressBuffer;
-            SSD1306::OledPoint location{0, 16};
-            drawString8x16(location, address, SSD1306::PixelStyle::Set, oled);
         }
     }
     if (ifAddrStruct != NULL)
--- users.cpp
+++ users.cpp
@@ -21,27 +21,40 @@
 #include <fstream>
 
 namespace { volatile static std::sig_atomic_t run = 1;}
-
+void splitString(std::string& input, std::string arr[], int& index);
 void showUsers(SSD1306::OledI2C& oled) {
-    std::string l = "N";
-    std::string j = "N";
     std::system("users | tr \" \" \"\n\" | uniq | tr \"\n\" \" \" > text.txt");
     std::ifstream File("text.txt");
     std::string text;
     std::getline (File, text);
     File.close();
-    if (text == "lucas ") {
-        l = "Y";
-    } else if (text == "josh ") {
-
-        j = "Y";
-    } else if (text == "lucas josh ") {
-        l = "Y";
-        j = "Y";
-    } else if (text == "josh lucas ") {
-        l = "Y";
-        j = "Y";
+    std::string arr[5];
+    int index = 0;
+    splitString(text, arr, index);
+    int num = 0;
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] != "") {
+            num++;
+        }
     }
-    drawString8x16(SSD1306::OledPoint{0, 32}, "L: " + l, SSD1306::PixelStyle::Set, oled);
-    drawString8x16(SSD1306::OledPoint{0, 46}, "J: " + j, SSD1306::PixelStyle::Set, oled);
+    for (int i = 0; i < num; i++){
+        int y = 32;
+        drawString8x16(SSD1306::OledPoint{0, y}, arr[i], SSD1306::PixelStyle::Set, oled);
+        y+=16;
+    }
+}
+
+void splitString(std::string& input, std::string arr[], int& index){
+    // Creating an input string stream from the input string
+    std::istringstream stream(input);
+
+    // Temporary string to store each token
+    std::string token;
+
+    // Read tokens from the string stream separated by the
+    // delimiter
+    while (std::getline(stream, token, ' ')) {
+        // Add the token to the array
+        arr[index++] = token;
+    }
 }
