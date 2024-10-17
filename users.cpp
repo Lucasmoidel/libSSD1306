5bbaf906541bc0329de886fe6882a50c52af813f
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-10-17 00:05:29 -0700

    a

--- users.cpp
+++ users.cpp
@@ -11,7 +11,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include "OledFont8x16.h"
+#include "OledFont8x8.h"
 #include "OledI2C.h"
 #include <ifaddrs.h>
 #include <sys/types.h>
@@ -21,36 +21,29 @@
 #include <fstream>
 
 namespace { volatile static std::sig_atomic_t run = 1;}
-void splitString(std::string& input, std::string arr[], int& index);
+void splitString(std::string& input, std::string arr[]);
 void showUsers(SSD1306::OledI2C& oled) {
     std::system("users | tr \" \" \"\n\" | uniq | tr \"\n\" \" \" > text.txt");
     std::ifstream File("text.txt");
     std::string text;
     std::getline (File, text);
     File.close();
-    std::string arr[5];
-    int index = 0;
-    splitString(text, arr, index);
-    int num = 0;
-    for (int i = 0; i < 5; i++) {
-        if (arr[i] != "") {
-            num++;
-        }
-    }
-    for (int i = 0; i < num; i++){
-        int y = 32;
-        drawString8x16(SSD1306::OledPoint{0, y}, arr[i], SSD1306::PixelStyle::Set, oled);
-        y+=16;
+    std::string arr[3];
+    splitString(text, arr);
+    int y = 32;
+    for (int i = 0; i < 3; i++){
+        drawString8x8(SSD1306::OledPoint{0, y}, arr[i], SSD1306::PixelStyle::Set, oled);
+        y+=9;
     }
 }
 
-void splitString(std::string& input, std::string arr[], int& index){
+void splitString(std::string& input, std::string arr[]){
     // Creating an input string stream from the input string
     std::istringstream stream(input);
 
     // Temporary string to store each token
     std::string token;
-
+    int index = 0;
     // Read tokens from the string stream separated by the
     // delimiter
     while (std::getline(stream, token, ' ')) {
